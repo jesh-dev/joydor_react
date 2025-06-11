1f47699266dc5cea7792e736a42f617c17dc4146
jesh-dev <jeshrunlaw@gmail.com>
2025-06-12 00:36:02 +0100

    decompromised

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ const App = () => {
                 {/* <Route path='' element={}/> */}
         <Route path="/paystack" element={<PaystackForm />} />
         
-        <Route path="/payment/success" element={<PaymentSuccess />} />
+        {/* <Route path="/payment/success" element={<PaymentSuccess />} /> */}
                 <Route path="/admin" element={ <AdminDashboard/> } />
             </Routes>
           </BrowserRouter>
